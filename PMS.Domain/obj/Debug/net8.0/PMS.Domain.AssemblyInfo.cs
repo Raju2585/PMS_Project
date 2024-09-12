18c0fdc15d31870e2da59544fe143cb2237e012f
PavaniMaddu <PavaniMaddu@DESKTOP-6TARQ1J>
2024-09-12 17:34:28 +0530

    merge

--- PMS.Domain/obj/Debug/net8.0/PMS.Domain.AssemblyInfo.cs
+++ PMS.Domain/obj/Debug/net8.0/PMS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3887323636e4a0cde819773efd3d09bbbfffa8a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf412bf94e7e6af88f2928d4d0125e0c8e156501")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
