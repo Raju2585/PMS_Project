e0c8a363f4dd28ac95be7741ef2db88b921a39a3
SanthirajuMerla <SanthirajuMerla@DESKTOP-SE2TGKQ>
2024-09-12 17:00:31 +0530

    merge

--- PMS.Application/obj/Debug/net8.0/PMS.Application.AssemblyInfo.cs
+++ PMS.Application/obj/Debug/net8.0/PMS.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c92339c70b8ccf2b954b9de33ed209b2f1c058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763507ed6e9d3255fb961014a68a44d8a617a505")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
