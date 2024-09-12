6c6b0dd9015f4c09215475c3a72ae5e5870eb88d
SanthirajuMerla <SanthirajuMerla@DESKTOP-SE2TGKQ>
2024-09-12 18:02:14 +0530

    changes commited

--- PMS.Infra/obj/Debug/net8.0/PMS.Infra.AssemblyInfo.cs
+++ PMS.Infra/obj/Debug/net8.0/PMS.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PMS.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e2ab2affd014a3b4b32a131295a3e402c8985b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da871558f156c00637b75e1137c418baf301305")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
